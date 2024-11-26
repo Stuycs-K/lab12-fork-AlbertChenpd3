e91f7aebfec750083329dd7009148ce40b5fe617
Albert Chen <achen51@stuy.edu>
2024-11-26 15:35:08 -0500

    get random

--- main.c
+++ main.c
@@ -3,21 +3,25 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <sys/types.h>
+#include <fcntl.h>
 
 int main(){
+  int randomFile = open("/dev/urandom",O_RDONLY,0644);
+  int * randomBuffer;
+  read(randomFile,randomBuffer,5);
+  printf(" %d about to create 2 child processes \n",getpid());
 
-  printf("%d about to create 2 child processes",getpid());
-  fork();
-  fork();
-  //Child processes hopefully
+
+  pid_t childA = fork();
+  //Child processes hopefully won't inherit parent's PID
   int x = rand() % 5;
-  printf("%d %d sec",getpid(),x);
+  printf(" %d %d sec \n",getpid(),x);
   sleep(x);
 
 
   //Waits hopefully for any child
-  wait(MACRO(WIFEXITED));
-  //Have to get child's pid from the parent function
-  printf("Main Process %d is done. Child %d slept for %d",getpid(),wait(),);
+  //Have to get child's pid from the parent function Probably use fork
 
+  printf(" Main Process %d is done. Child %d slept for %d \n",getpid(), childA,x);
+  printf("%d",*randomBuffer);
 }
